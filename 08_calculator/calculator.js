fdf2e73ec920b45606494035bee6fd9d8a0264a6
axruan <42079946+axruan@users.noreply.github.com>
2024-08-23 17:19:51 -0400

    Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,25 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return +num1 + +num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return +num1 - +num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, curr) => total + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.length == 0 ? 0 : arr.reduce((total, curr) => total * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return Math.pow(num, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  return num <= 1 ? 1 : num * factorial(num - 1);
 };
 
 // Do not edit below this line
