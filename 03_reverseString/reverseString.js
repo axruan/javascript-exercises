3b1fe66cc27bea7ddb99e6564957f6acddf1f2cd
axruan <42079946+axruan@users.noreply.github.com>
2024-08-22 21:44:33 -0400

    Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(s) {
+    const charArray = s.split('');
+    let string = '';
+
+    for (let i = charArray.length - 1; i > -1; i--) {
+        string += charArray[i];
+    }
+
+    return s == null ? '' : string;
 
 };
 
