9f75710a5eb5a2045193efcd4b91cc1793824af4
axruan <42079946+axruan@users.noreply.github.com>
2024-08-22 21:25:15 -0400

    Exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, repeat) {
+    if (repeat < 0) {
+        return "ERROR";
+    }
 
+    let repeatedString = string;
+
+    for (let i = 1; i < repeat; i++) {
+        repeatedString = repeatedString + string;
+    }
+
+    return repeat == 0 ? "" : repeatedString;
 };
 
 // Do not edit below this line
