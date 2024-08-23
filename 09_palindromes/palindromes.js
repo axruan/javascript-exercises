51d643508cd93d4c89c2e766234b25d6ed0facba
axruan <42079946+axruan@users.noreply.github.com>
2024-08-23 17:54:41 -0400

    Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.replace(/[^a-z0-9]/gi, '').toLowerCase(); 
+    const mid = str.length % 2 === 0 ? str.length / 2 : Math.floor(str.length) + 1
+    
+    for (let i = 0; i < mid; i++) {
+      if (str.charAt(i) !== str.charAt(str.length - 1 - i)) {
+        return false;
+      }
+    }
 
+    return true;
 };
 
 // Do not edit below this line
