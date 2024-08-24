bdffee2e7386a60014a66c86e27431efb9b66921
axruan <42079946+axruan@users.noreply.github.com>
2024-08-23 20:54:12 -0400

    Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = parseInt(n);
+    let a = 0;
+    let b = 1;
+    let sum = 0;
 
+    if (n === 0) {
+        return a;
+    } else if (n < 0) {
+        return "OOPS";
+    }
+
+    for (let i = 2; i < n + 1; i++) {
+        sum = a + b;
+        a = b;
+        b = sum;
+    }
+
+    return b;
 };
 
 // Do not edit below this line
